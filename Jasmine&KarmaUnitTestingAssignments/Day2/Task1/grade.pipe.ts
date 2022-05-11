ccc4972c8f9eeb754a34169be5b5e8ff44cccbdc
SnehaBondre007 <104135084+SnehaBondre007@users.noreply.github.com>
2022-05-11 15:07:37 +0530

    Add files via upload

---
+++ Jasmine&KarmaUnitTestingAssignments/Day2/Task1/grade.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'gradepipe'
+})
+export class GradePipe implements PipeTransform {
+
+  transform(input:number): string {
+    let output:string ="";
+
+    if(input == 1) output = "Outstanding";
+    if(input == 2) output = "Excellent";
+    if(input == 3) output = "Good";
+    if(input == 4) output = "Average";
+    if(input == 5) output = "Poor";
+
+
+    return output;
+  }
+
+}
